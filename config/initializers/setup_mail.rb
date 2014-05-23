f8e5f028d9b2f5c0e15a0131407e742c395b57c0
Brian Boyd <brian@cubicalweb.com>
2014-05-23 05:47:07 -0400

    testing config change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,10 @@
 ActionMailer::Base.delivery_method = :smtp
-config.action_mailer.default_url_options = { :host => 'cubicalwebbiz.herokuapp.com/' }
 ActionMailer::Base.smtp_settings = {
   :address              =>  'smtp.sendgrid.net',
   :port                 =>  '587',
   :authentication       =>  :plain,
   :user_name            =>  ENV['SENDGRID_USERNAME'],
   :password             =>  ENV['SENDGRID_PASSWORD'],
-  :domain               =>  'heroku.com',
+  :domain               =>  ENV['SENDGRID_DOMAIN'],
   :enable_starttls_auto =>  true
 }
